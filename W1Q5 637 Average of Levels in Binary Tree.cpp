8031d8b2d6260a9be8fd19af755d9d3c1d3db161
Divyansh Rajput <35797156+divyanshr1@users.noreply.github.com>
2021-03-05 15:53:47 +0530

    Add files via upload

---
+++ W1Q5 637 Average of Levels in Binary Tree.cpp
@@ -0,0 +1,38 @@
+http://vesapmys-82.ad.infosys.com:8000/saptools/#   RESET SERVER CREDENTIALS
+
+
+SE24 ---> Create Class
+
+Assignment:
+-----------
+While creating materials assign plant,storage location, sales organization, distribution channel, and division.
+Create raw materials Hard disk and mouse
+Item category for raw materials should be NORM
+Crate finished product Desktop
+Item Category for Finished Product should be ERLA
+Create customer Eazy Enterprises
+
+Create CMIR using customer Eazy Enterprises and list all raw materials and finished products
+Create following Condition Master Records and conditions should be valid for 2 years from the current date
+for raw materials.
+    For Material Hard Disk:
+        Price should be 700 INR
+    for Material Mouse:
+        Price should be 400 INR
+    for Material Desktop:
+        
+        price should be valid from 11.01.2021 to 11.03.2021     
+            1 to 50  30000
+            51 to 100  29500
+            above 100 29000
+        12.03.2021 to 11.03.9999  31000
+Create Bill of Material and asiign raw materials for the finished product Desktop.
+Give quantity as 4 for each raw material
+Enter the materials into inventory with quantity as 500
+Create sales order and explode the BOM.
+Create outbound delivery and post goods issue
+Check available quantity of materials in MMBE t code
+
+9671692434 :- Kalua
+
+qwertyuiopasdfghjkl;'zxcvbnmnbvvcxzA	`11234556789020.
